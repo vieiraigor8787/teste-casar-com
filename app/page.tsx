7c6211727d417546962210c1e7005e17acd0ceff
Igor Cerqueira <igor.cerqueira@jussi.local>
2024-11-05 12:52:46 -0300

    configuraçao tailwind

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-greyNeutral text-xl">hello world</h1>
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        primaryLight: 'var(--primaryLight)',
+        primaryDark: 'var(--primaryDark)',
+        placeholderText: 'var(--placeholderText)',
+        greyNeutral: 'var(--greyNeutral)',
+        greyDark: 'var(--greyDark)',
+        greySecondary: 'var(--greySecondary)',
+        background: 'var(--white)',
+        backgroundMatte: 'var(--backgroundMatte)',
+        success: 'var(--success)',
+        warning: 'var(--warning)',
+        danger: 'var(--danger)',
+      },
+      fontSize: {
+        xl: '21px',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
